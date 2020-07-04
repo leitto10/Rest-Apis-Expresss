05027d57e63953f05f7de34656279cf4666bdb00
leitto10 <46364179+leitto10@users.noreply.github.com>
2020-07-04 02:40:08 -0700

    adding records file.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const records = require('./records');
+
+//Send a GET request to READ(view) a list of quotes
+app.get('/quotes', async (req, res) => {
+    const quotes = await records.getQuotes();
+    res.json(quotes);  
+})
+
+//Send a GET request to READ(view) a single quote
+app.get('/quotes/:id', async (req, res) => {
+    const quote = await records.getQuote(req.params.id);
+    res.json(quote);
+})
+//Send a POST request to CREATE a quote
+//Send a PUT request to UPDATE(edit) a quote
+//Send a DELETE request to DELETE a quote
+
+
+app.listen(3000, () => console.log('Quote API listening on port 3000!'));
