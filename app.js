4995b5373f24b7fff5b1b6fd57ac890ec9d8801f
leitto10 <46364179+leitto10@users.noreply.github.com>
2020-11-27 01:16:39 -0800

    adding api routes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 const records = require('./models/quotes');
 
 
-app.get('/api', (req, res, next) => {
+app.get('/api/quotes', (req, res, next) => {
     return records.find({})
     .lean()
     .then((quotes) => {
